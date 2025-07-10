7192db19b75154dfd241edc00ee13befc53434a4
Silvia Baffi <sil.baffi@gmail.com>
2025-07-10 17:37:43 +0200

    Aggiunta esercizi async/await e emitter

--- es-asyncawait.mjs
+++ es-asyncawait.mjs
@@ -1,4 +1,4 @@
-// Create agetResults function that uses async and await. Inside of the function,
+// Create a getResults function that uses async and await. Inside of the function,
 // call the luckyDraw function for each of the players: Tina, Jorge, Julien.
 // Log out the resolved value for each promise and handle any promise rejections.
 
