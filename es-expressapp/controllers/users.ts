67f3220c8eb91b2bb7b498932cc718ba348380ba
Silvia Baffi <sil.baffi@gmail.com>
2025-07-24 15:52:21 +0200

    Aggiunta esercizi JWT 2 e 3

--- es-expressapp/controllers/users.ts
+++ es-expressapp/controllers/users.ts
@@ -35,7 +35,7 @@ export async function logIn(req: Request, res: Response) {
 export async function logOut(req: Request, res: Response) {
   const user: any = req.user;
 
-  await db.none("UPDATE users SET token = $2 WHERE id = $1", [user?.id, null]);
+  await db.none("UPDATE users SET token = NULL WHERE id = $1", [user?.id, null]);
 
   res.status(200).json({ message: "Logout effettuato con successo." });
 }
